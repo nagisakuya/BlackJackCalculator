655f48c45f5a51c41b06baa13f82ac797aff55ee
nagisakuya <nagisakuya@gmail.com>
2020-09-29 23:11:26 +0900

    提出できるレベルに完成 ただ、まだ改善点はアリ

--- BlackJack/Main_BlackJackCalculattor.cpp
+++ BlackJack/Main_BlackJackCalculattor.cpp
@@ -1,20 +1,12 @@
 ﻿
 #include <iostream>
-#include "BlackJack.h"
 #include "BlackJackCalculator.h"
-#include<fstream>
+#include <fstream>
 
 using namespace nagisakuya::BlackJack;
 using namespace std;
 
-/*int main()
-{
-	srand((unsigned int)time(NULL));
-	Table test(8, Rule());
-	string temp_s;
-	test.addplayer(Player(1));
-	test.replay();
-}*/
+
 
 int main()
 {
